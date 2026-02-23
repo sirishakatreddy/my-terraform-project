a6e1ef760eeb5e3a1767ea2e15b686456a94bb1e
SirishaKatreddy <146175317+sirishakatreddy@users.noreply.github.com>
2026-02-23 07:40:15 +0530

    Add variables.tf for VPC and EC2

---
+++ project-vpc-ec2/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "subnet_cidr_1" {
+  description = "Subnet CIDR 1"
+  type        = string
+}
+
+variable "subnet_cidr_2" {
+  description = "Subnet CIDR 2"
+  type        = string
+}
+
+variable "instance_ami" {
+  description = "EC2 instance ami"
+  type = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "state_bucket_name" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
