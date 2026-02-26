756dc3c80e31607681f6b5226af7fb20fa16fad5
SirishaKatreddy <146175317+sirishakatreddy@users.noreply.github.com>
2026-02-26 18:19:42 +0530

    Update userdata2.sh

--- project-vpc-ec2/userdata2.sh
+++ project-vpc-ec2/userdata2.sh
@@ -4,4 +4,4 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-echo "<h1>Web Server 1</h1><p>Created using Terraform user_data</p>" > /var/www/html/index.html
+echo "<h1>Web Server 2</h1><p>Created using Terraform user_data</p>" > /var/www/html/index.html
